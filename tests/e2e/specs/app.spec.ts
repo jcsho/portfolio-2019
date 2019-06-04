9072f06564c0c59d34a080334efe069a6643a989
Justin Ho <justinhodev@gmail.com>
2019-06-04 01:07:07 -0700

    Add page objects
    add oop logic by separating page testing into a class

--- tests/e2e/specs/app.spec.ts
+++ tests/e2e/specs/app.spec.ts
@@ -1,12 +1,31 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-type Url = string;
+/**
+ * Model for the root page
+ */
+class HomePage {
+  /** Visit the root url */
+  public visit() {
+    cy.visit('/');
+  }
+
+  /** Get page logo */
+  get logo() {
+    return cy.get('.nav__logo');
+  }
+
+  /** Get navbar instance */
+  get navbar() {
+    return cy.get('nav');
+  }
+}
+
+const home = new HomePage();
 
 describe('My Portfolio Site', () => {
   it('Visit the app root url', () => {
-    const url: Url = '/';
-    cy.visit(url);
-    cy.contains('h1', 'Justin Ho');
+    home.visit();
+    home.logo.contains('Justin Ho');
   });
 
   it('Has a project page called Mesh', () => {
