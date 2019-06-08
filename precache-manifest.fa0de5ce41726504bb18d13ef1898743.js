483e06843f8fd5ce6569614aecac945a3540d5b2
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-06-08 17:49:58 +0000

    Deploy justinhodev/portfolio to github.com/justinhodev/portfolio.git:gh-pages

---
+++ precache-manifest.fa0de5ce41726504bb18d13ef1898743.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "0553593fa5da7f13ca81c69c5ca6b84e",
+    "url": "/portfolio/img/phase-1-prototype-compressed.0553593f.png"
+  },
+  {
+    "revision": "751c6a056e6565234b4a2888277a9c4d",
+    "url": "/portfolio/img/social-preview.png"
+  },
+  {
+    "revision": "8cdbfa4ea906c3f68361",
+    "url": "/portfolio/js/chunk-vendors.a3feb51a.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/portfolio/robots.txt"
+  },
+  {
+    "revision": "8d12114d93eddabe5843",
+    "url": "/portfolio/js/hypetracker.b61a9eb0.js"
+  },
+  {
+    "revision": "1bf6c6fa94b1f8771712c0e55ba9ceac",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "31f71a33f55be1f48763",
+    "url": "/portfolio/js/mesh.a56df956.js"
+  },
+  {
+    "revision": "99554906fd54662d4be6463123f6ed69",
+    "url": "/portfolio/img/vsl-logo-min.99554906.png"
+  },
+  {
+    "revision": "07a38a7e64787d35ff92",
+    "url": "/portfolio/js/nycrime.a72ca477.js"
+  },
+  {
+    "revision": "03858dd4b3593d0e44ba",
+    "url": "/portfolio/js/app.7bcd6b91.js"
+  },
+  {
+    "revision": "d18688be6e16953cedca",
+    "url": "/portfolio/js/vsl.eddac5ba.js"
+  },
+  {
+    "revision": "07a38a7e64787d35ff92",
+    "url": "/portfolio/css/nycrime.c67fdde5.css"
+  },
+  {
+    "revision": "e992aea44c16f6969c21055d76ea63a9",
+    "url": "/portfolio/img/ny-crime-vis-compressed.e992aea4.png"
+  },
+  {
+    "revision": "e5e79f29cab89216ddd9c8b7080528fb",
+    "url": "/portfolio/img/final-prototype-compressed.e5e79f29.png"
+  },
+  {
+    "revision": "608c96d7632e7e93e3182a8ce81b0b27",
+    "url": "/portfolio/img/merging-sketch-2-compressed.608c96d7.png"
+  },
+  {
+    "revision": "266e4354a43153310eaefa58c932351b",
+    "url": "/portfolio/img/nested-alternative-compressed.266e4354.png"
+  },
+  {
+    "revision": "77a75c36edae7efa97fdefed348b6f05",
+    "url": "/portfolio/img/mesh-icon-compressed.77a75c36.png"
+  },
+  {
+    "revision": "f8cbb80ca3402e9cffc1f0503da266f8",
+    "url": "/portfolio/img/merging-sketch-1-compressed.f8cbb80c.png"
+  },
+  {
+    "revision": "1e3b33cba023ef275591ecb2adf3e26f",
+    "url": "/portfolio/img/initial-prototype-compressed.1e3b33cb.png"
+  },
+  {
+    "revision": "1a5bcd599fc66c9bdbed9f1e538fbcb0",
+    "url": "/portfolio/img/details-page-cropped-compressed.1a5bcd59.png"
+  },
+  {
+    "revision": "d18688be6e16953cedca",
+    "url": "/portfolio/css/vsl.3e3b638a.css"
+  },
+  {
+    "revision": "31f71a33f55be1f48763",
+    "url": "/portfolio/css/mesh.cf3b29ef.css"
+  },
+  {
+    "revision": "8d12114d93eddabe5843",
+    "url": "/portfolio/css/hypetracker.c67fdde5.css"
+  },
+  {
+    "revision": "03858dd4b3593d0e44ba",
+    "url": "/portfolio/css/app.a71c72e6.css"
+  }
+];
