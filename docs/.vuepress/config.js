907ffd9f943eef4150f158375c5ac1fb5b06c867
Justin Ho <justinhodev@gmail.com>
2019-06-09 00:20:19 -0700

    Configure Pages
    add home and usage pages,
    changed default styling

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,11 +1,23 @@
 module.exports = {
-  title: 'Design - Justin Ho',
-  description: 'Personal Playground and Design System',
   configureWebpack: {
     resolve: {
       alias: {
         '@': '../src'
       }
     }
+  },
+  title: 'Design - Justin Ho',
+  description: 'Personal Playground and Design System',
+  head: [
+    ['link', {rel: 'icon', href: '/favicon.ico'}]
+  ],
+  themeConfig: {
+    repo: 'justinhodev/portfolio',
+    repoLabel: 'Github',
+    docsDir: 'docs',
+    sidebar: [
+      '/',
+      '/usage'
+    ]
   }
 }
