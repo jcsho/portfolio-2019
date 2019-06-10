6cc1c5b4cc963b376deb57070ee74e0775675676
Justin Ho <justinhodev@gmail.com>
2019-06-10 15:57:32 -0700

    Change Sass Config
    add vue loader option to load in global main sass file for variables, mixins, functions, etc

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,12 @@
+const path = require("path");
+
 module.exports = {
   productionSourceMap: false,
-  publicPath: process.env.BASE_URL
+  publicPath: process.env.BASE_URL,
+  pluginOptions: {
+    "style-resources-loader": {
+      preProcessor: "scss",
+      patterns: [path.reesolve(__dirname, "./src/styles/main.scss")]
+    }
+  }
 }
